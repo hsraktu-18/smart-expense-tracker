d5853f4ca1d6cff37fc1c00c9d9b5a28effcae37
hsraktu-18 <udave5899@gmail.com>
2026-01-30 22:17:11 +0530

    Added User entity with PostgreSQL mapping

---
+++ src/main/java/com/utkarsh/entity/User.java
@@ -0,0 +1,77 @@
+package com.utkarsh.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity   // tells Spring: this class = database table
+@Table(name = "users") // table name in DB
+public class User {
+
+    @Id  // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;   // unique number for each user
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    // Constructor (empty)
+    public User() {}
+
+    // Constructor (for easy object creation)
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    // Getters & Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
