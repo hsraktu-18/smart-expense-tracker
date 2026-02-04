2c7775c63be947e91731c869051c87e655f328e0
hsraktu-18 <udave5899@gmail.com>
2026-02-04 13:38:46 +0530

    Fix LoginRequestDto for JSON deserialization

--- src/main/java/com/utkarsh/dto/LoginRequestDto.java
+++ src/main/java/com/utkarsh/dto/LoginRequestDto.java
@@ -1,5 +1,12 @@
 package com.utkarsh.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDto {
 
     private String email;
