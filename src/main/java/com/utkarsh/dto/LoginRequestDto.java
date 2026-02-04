00fa5270f2f9a98f222f08997870bca498e7b3da
hsraktu-18 <udave5899@gmail.com>
2026-02-04 13:56:33 +0530

    Fix LoginRequestDto deserialization issue

--- src/main/java/com/utkarsh/dto/LoginRequestDto.java
+++ src/main/java/com/utkarsh/dto/LoginRequestDto.java
@@ -12,11 +12,5 @@ public class LoginRequestDto {
     private String email;
     private String password;
 
-    public String getEmail() {
-        return email;
-    }
 
-    public String getPassword() {
-        return password;
-    }
 }
